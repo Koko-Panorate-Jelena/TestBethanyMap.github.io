e1493abd9205bcd0cd904df06624c96716285b43
Jelena i Kosta <150233848+Koko-Panorate-Jelena@users.noreply.github.com>
2023-11-20 13:50:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-// map = new google.maps.Map (document.getElementById('map'), {
-//    center: {lat:-43.8859427464520, lng:20.356899288107964},
-//   zoom: 8,
-//    mapId: '8373531fef93e428'
-// });
 
 
 // 43.88594274645201, 20.356899288107964
@@ -30,12 +25,15 @@ function initMap() {
 
   const directionsRenderer = new google.maps.DirectionsRenderer();
   const directionsService = new google.maps.DirectionsService();
+  let sourceAutocomplete, desAutocomplete;
 
   const myLatLng = { lat: 40.20502830903152, lng: -80.55853842938608 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 17,
     mapId: "fe1884329fa294ee",
     center: myLatLng,
+    // disableDefaultUI: true
+    // mapTypeId: google.maps.MapTypeId.HYBRID
   });
 
   directionsRenderer.setMap(map);
@@ -45,150 +43,176 @@ function initMap() {
   });
 
 
+  // KOD JE DOBAR ZA AUTOCOMPLETE OBA POLJA, ALI NE UCITAVA MARKERE
+  sourceAutocomplete = new google.maps.places.Autocomplete (document.getElementById('from'));
+  desAutocomplete = new google.maps.places.Autocomplete (document.getElementById('to'));
+
+  function calcRoute() {
+    var source = document.getElementById('from').value;
+    var dest = document.getElementById('to').value;
+    var request = {
+      origin: source,
+      destination: dest,
+      travelMode: "DRIVING"
+    };
+  
+    directionsService.route(request, function (result, status) {
+      if (status == "OK") {
+        directionsRenderer.setDirections(result);
+      }
+    });
+  }
+  document.getElementById("calculateRouteBtn").addEventListener("click", function () {
+    calcRoute();
+  });
+
+
+
+
   const markers = [
     [
-      "Church",
-      40.2048316174204,
-      -80.55808664082726,
-      "icons8-church-100.png",
-      70,
-      70,
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+      "Grace Phillips Johnson Visual Arts Center",
+      40.20622905066137,
+      -80.56075030444904,
+      "images/FINE-ARTS.png",
+      60,
+      60,
+      "Grace Phillips Johnson Visual Arts Center (1984) offers facilities for television, painting, sculpture, and design. The Sandra Weiss Berkman Studio for Ceramic Arts is attached to the Center and Bethany's on - campus television studio, located on the ground floor."
     ],
     [
       "Richardson Hall of Science",
-      40.205638937901476,
-      -80.56096129301824,
-      "icons8-atom-100.png",
-      70,
-      70,
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus"
+      40.205654215090995,
+      -80.5609670732336,
+      "images/Hall-Of-Science.png",
+      60,
+      60,
+      "Richardson Hall of Science provides facilities for the Chemistry, Physics, and Mathematics departments and houses the Math Learning Center and Academic Computing Center. It is named for Robert Richardson, Bethany’s first science professor."
     ],
     [
-      "Health and Wellness Center",
-      40.20541881270227,
-      -80.56342335652832,
-      "icons8-health-100.png",
-      70,
-      70,
-      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish."
+      "T.W. Phillips Memorial Library",
+      40.20533230504398,
+      -80.56187027658983,
+      "images/LIBRARY.png",
+      60,
+      60,
+      "T. W. Phillips Memorial Library serves as the hub of an academic information network that provides the campus with over 250 000 itemslocally (books, periodicals, newspapers, audiovisuals, archival materials) and access to information through online subscriptions to ProQuest Direct, Lexis-Nexis Universe, JSTOR, Britannica Online, and many other sources. The library is home to the Mary Cutlip Center for Library and Information Technology Services, which provides technical and information services in both traditional library and digital forms."
     ],
     [
-      "Post Office",
-      40.2068678386469,
-      -80.55663873814818,
-      "icons8-letter-100.png",
-      70,
-      70,
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt."
+      "Goodnight House",
+      40.20621065600143,
+      -80.56380915317803,
+      "images/DORM-1.png",
+      60,
+      60,
+      "Goodnight House is a two-story co-ed residence hall designed to promote community living. Residents live in cubes consisting of four bedrooms and a common bathroom. Each bedroom contains two single beds, dressers, closet space and a desk with chair. Goodnight features two large lounge spaces with a television located on the upper level. The residence is also equipped with a common kitchen and laundry facility."
     ],
     [
       "Steinman Fine Arts Center",
-      40.20658713970493,
-      -80.55907245923623,
-      "icons8-krita-100.png",
-      70,
-      70,
-      "Jelena i Kostas se vrlo lepo zabavljaju dok rade ovaj projekat. Jelena vise neko Koki."
+      40.20660614014581,
+      -80.55952194788456,
+      "images/FINE-ARTS.png",
+      60,
+      60,
+      "David and Irene Steinman Fine Arts Center provides excellent facilities for music and theatre. A fully equipped theatre occupies the central portion of the building. Teaching studios, studio-classrooms, general rehearsal room for the larger vocal and instrumental groups, and individual practice rooms support instruction in music."
     ],
     [
-      "Bethany College",
+      "Bethany College - Old Main",
       40.20606367100533,
       -80.55985721011768,
-      "icons8-museum-100.png",
-      90,
-      90,
-      "Welcome to Bethany College!"
+      "images/OLD-MAIN.png",
+      60,
+      60,
+      "Built in 1858, Old Main is the centerpiece of Bethany's campus and academic buildings. The building is one of the earliest examples of collegiate Gothic architecture in the United States and, as such, is listed in the National Register of Historic Places."
     ],
     [
       "Hoag Soccer Field",
       40.20226299388096,
       -80.56546549660865,
-      "icons8-soccer-ball-100.png",
-      70,
-      70,
-      "Hoag Soccer Field"
+      "images/SOCCER.png",
+      60,
+      60,
+      "Hoag Field was originally built in 1982 and is one of the most aesthetic soccer complexes in the Eastern United States. The home of the 1994 men's national championship team, as well as the five-time PAC champion women's soccer program, Hoag Field has hosted numerous NCAA Tournament men's soccer matches, including the epic 1994 NCAA Quarterfinal match against UC-San Diego and all three of Bethany's first round matchups in their recent trips to the playoffs."
     ],
     [
-      "Bison Stadium",
+      "Bison Stadium - Coach Don Ault Athletic Field",
       40.2038374090133,
       -80.56609951812203,
-      "icons8-american-football-ball-100.png",
-      70,
-      70,
-      "Bison Stadium"
+      "images/AMERICAN-FOOTBALL.png",
+      60,
+      60,
+      "Bison Stadium underwent significant renovations in the summer of 2021. Changes included the installation of new Field Turf, lighting, a rubberized track, steeplechase pit, and facilities for various track and field events. These upgrades have opened the facility for use by the entire Bethany community, including varsity soccer and lacrosse teams, intramural programs, and local walkers."
     ],
     [
-      "Bison Softball Field",
+      "Softball Field",
       40.2037311939913,
       -80.56432141169445,
-      "icons8-baseball-100.png",
-      70,
-      70,
-      "Bison Softball Field"
+      "images/BASEBALL.png",
+      60,
+      60,
+      "The softball field is home Bethany’s softball team, who boasts an impressive 13 PAC championships and six appearances in the NCAA Tournament."
     ],
     [
-      "Hummel Field House",
-      40.20475003928364,
-      -80.56568918854151,
-      "icons8-basketball-net-100.png",
-      70,
-      70,
-      "Basketball Court"
+      "Campbell Village",
+      40.20612328128562,
+      -80.56573598700466,
+      "images/DORM-1.png",
+      60,
+      60,
+      "Campbell Village is a four-building complex, housing 380 students in suite style living. These apartment - style suites provide the most modern and comfortable accommodations on-campus. The suites are designed for four visitors, each with his or her own bedroom. All bedrooms are furnished with a double bed, dresser, nightstand, mirror/closet, desk, and chair. In each suite visitors share a living area, two bathrooms (one for every two visitors), and a kitchenette (full-sized refrigerator, microwave, sink, and a table and chairs)."
     ],
     [
       "Parking Coal Bowl",
       40.206115799497255,
       -80.5623772158248,
-      "icons8-parking-ticket-100.png",
-      70,
-      70,
+      "images/PARKING.png",
+      60,
+      60,
       "Parking Lot"
     ],
     [
       "Harlan Hall",
-      40.204803743935734,
-      -80.56112780590456,
-      "icons8-city-100.png",
-      70,
-      70,
-      "Student Dorm"
+      40.20465543868551,
+      -80.56157684144388,
+      "images/DORM-1.png",
+      60,
+      60,
+      "The recently renovated Harlan Hall, located in the center section of campus, serves as a freshman residence hall with communal style living for 58 students. Harlan Hall features large double bedrooms equipped with two single beds, dressers, closet space, and desks with chairs. Shared bathroom facilities are located on each floor."
     ],
     [
-      "Judith R. Hurl Education Center",
-      40.20771909749787,
-      -80.55604903625049,
-      "icons8-education-100.png",
-      70,
-      70,
-      "Educational Center"
+      "Phillips Hall",
+      40.20485778434248,
+      -80.56063750995341,
+      "images/Hall-2.png",
+      60,
+      60,
+      "Phillips Hall is the all-in-one campus hub with a welcome center for visitors to campus and a student - centric lounge."
     ],
     [
-      "Bethany House",
-      40.20541573732268,
-      -80.55776178523949,
-      "icons8-opened-folder-100.png",
-      70,
-      70,
-      "Administrative Center"
+      "Cramblet Hall",
+      40.20487617937225,
+      -80.55980656286567,
+      "images/Hall-2.png",
+      60,
+      60,
+      "Cramblet Hall was constructed in 1909 through a gift from Andrew Carnegie. Originally the library, it was remodeled in 1961 to house administrative offices. It is named in honor of two Presidents of the College, Thomas E. Cramblet and his son, Wilbur Haverfield Cramblet."
     ],
     [
       "Ogden Dining Hall",
       40.205883770657785,
       -80.55806578439638,
-      "icons8-tableware-100.png",
-      70,
-      70,
-      "Dining & Student Spaces"
+      "images/DINING.png",
+      60,
+      60,
+      "Ogden Dining Hall offers a variety of culinary delights. It features a pizza oven, a sandwich station, and a soft-serve ice-cream machine for sweet treats. Additionally, you can enjoy a daily hot-food menu with diverse options, making it a satisfying dining destination for all."
     ],
     [
-      "Bethany Park",
-      40.2021124004575,
-      -80.5529848825885,
-      "icons8-path-100.png",
-      70,
-      70,
-      "Park"
+      "Bethany House",
+      40.20545562025743,
+      -80.55772919510153,
+      "images/ADMIN-HOUSE.png",
+      60,
+      60,
+      "Bethany House features a variety of essential offices and spaces. The Center for Enrollment assists prospective students on their educational journeys. The Office of Student Life fosters a vibrant campus community, connecting students and enabling extracurricular pursuits. The Department of Communications and Media Arts offers educational opportunities for those passionate about media and communication. The Renner Art Gallery serves as a creative hub, showcasing the talent and imagination of artists, both students and professionals alike."
     ]
 
   ];
@@ -215,6 +239,25 @@ function initMap() {
     const infowindow = new google.maps.InfoWindow ({
       content: currMarker[6],
     });
+
+
+    // Current Location of Users (if Geolocation is enabled)
+
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(function (p) {
+    //     let position = {
+    //       lat: p.coords.latitude,
+    //       lng: p.coords.longitude
+    //     };
+    //     infowindow.setPosition(position);
+    //     infowindow.setContent("Your Location");
+    //     infowindow.open(map);
+    //   }, function () {
+    //     handleLocationError("Geolocation Service Failed", map.center());
+    //   })
+    // } else {
+    //   handleLocationError("No geolocation available", map.center());
+    // }
   
     // marker.addListener("click", () => {
     //   infowindow.open(map,marker);
@@ -240,10 +283,42 @@ function initMap() {
       currentInfoWindow = null;
     }
   });
+
+
+  // EARTHQUAKES - DOESN'T WORK
+  // var script = document.createElement('script');
+  // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+  // document.getElementsByTagName('head')[0].appendChild(script);
+
+  // function eqfeed_callback (geojson) {
+  //   map.data.addGeoJson(geojson);
+
+  //   map.data.setStyle(function(feature) {
+  //     var magnitude = feature.getProperty('mag');
+  //     return {
+  //       icon: getEarthquakeCircle(magnitude)
+  //     };
+  //   });
+  // }
+
   
-
-
+  
 }
+
+
+  // EARTHQUAKES2 - DOESN'T WORK
+// function getEarthquakeCircle (value) {
+//   return {
+//     path: google.maps.SymbolPath.CIRCLE,
+//     fillColor: 'purple',
+//     fillOpacity: .2,
+//     scale: Math.pow(2, value) / 2,
+//     strokeColor: 'white',
+//     strokeWeight: .5
+//   };
+// }
+
+
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer){
   const selectedMode = document.getElementById("mode").value;
@@ -260,9 +335,93 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer){
     directionsRenderer.setDirections(response);
   })
   .catch((e) => window.alert("Direction request failed due to" + status));
+
+
+
+
+  var input1 = document.getElementById("from");
+  var input2 = document.getElementById("to");
+
+  document.addEventListener("DOMContentLoaded", function (){
+    var autocomplete = new google.maps.places.Autocomplete(input1, input2, {
+      types: ['geocode']
+    });
+    autocomplete.addListener("place_changed", function() {
+      var near_place = autocomplete.getPlace(); 
+    });
+  });
+  
+
+
 }
 
+
+
+
+
 window.initMap = initMap;
+
+
+
+
+
+
+
+
+
+
+// autocomplete objects for all inputs
+
+// let options = {
+//   types: ["(addresses)"]
+// }
+
+// let input1 = document.getElementById("from");
+// let searchBox = new google.maps.places.SearchBox(input1);
+
+// let input2 = document.getElementById("to");
+// let autocomplete2 = new google.maps.places.Autocomplete(input2);
+
+
+// map.addListener('bounds_changed', function() {
+//   searchBox.setBounds(map.getBounds());
+// });
+
+// let markers = [];
+
+// searchBox.addListener('places_changed', function () {
+//   let places = searchBox.getPlaces();
+
+//   if (places.length === 0)
+//     return;
+
+//   markers.forEach(function (m) { m.setMap(null); });
+//   markers = [];
+
+//   let bounds = new google.maps.LatLngBounds();
+//   places.forEach(function(p) {
+//     if (!p.geometry)
+//       return;
+
+//     markers.push(new google.maps.Marker({
+//       map,
+//       title: p.name,
+//       position: p.geometry.location
+//     }));
+
+//     if (p.geometry.viewport)
+//       bounds.union(p.geometry.viewport);
+//     else
+//       bounds.extend(p.geometry.location);
+//   });
+  
+//   map.fitBounds(bounds);
+// });
+
+
+
+
+
 
 
 
@@ -336,4 +495,6 @@ window.initMap = initMap;
 // }
 
 // initMap();
+
+
 
